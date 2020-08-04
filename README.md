da6ca4eeac4d2195260920f4901efe57b7185fa9
SwimmingTiger <hu60.cn@gmail.com>
2020-08-04 17:57:47 +0800

    初次提交

---
+++ README.md
@@ -0,0 +1,26 @@
+# 通过 php-webdriver (Selenium) 进行视频文件嗅探的例子
+
+## 安装
+
+1. PHP-CLI
+2. Chrome
+3. [Chrome Driver](https://sites.google.com/a/chromium.org/chromedriver/downloads)
+
+## 配置
+
+打开`run.php`，修改`Chrome driver`的路径。
+
+## 运行
+
+```
+php run.php 2>&1 | tee run.log
+```
+
+## 结果
+
+查看 [run.log](run.log)。
+
+## 相关链接
+
+* [php-webdriver](https://github.com/php-webdriver/php-webdriver)
+* [Selenium](https://www.selenium.dev/)
---
+++ run.php
@@ -0,0 +1,71 @@
+<?php
+namespace Facebook\WebDriver;
+
+use Facebook\WebDriver\Chrome\ChromeDriver;
+use Facebook\WebDriver\Chrome\ChromeDriverService;
+use Facebook\WebDriver\Chrome\ChromeOptions;
+
+require_once('vendor/autoload.php');
+
+// 视频网址
+$url = 'https://www.bilibili.com/video/BV1UK4y147XT';
+
+// 要寻找的多媒体文件网址（正则表达式）
+$findUrl = '/\.mp4\b/i';
+
+// 播放按钮的css选择器
+$playButtonSelector = '.player-icon';
+
+// Chrome driver
+// https://sites.google.com/a/chromium.org/chromedriver/downloads
+$bin = '/usr/local/bin/chromedriver';
+$port = 9515;
+
+// 设为手机浏览器
+$options = new ChromeOptions();
+$mobileEmulation = ['deviceName' => 'iPhone X'];
+$options->setExperimentalOption('mobileEmulation', $mobileEmulation);
+
+// 开启性能记录
+$caps = $options->toCapabilities();
+$caps->setCapability('loggingPrefs', ['performance' => 'INFO']);
+
+$service = new ChromeDriverService($bin, $port);
+$driver = ChromeDriver::start($caps, $service);
+
+$driver->get($url);
+
+// 等待播放按钮出现
+$driver->wait(10, 1000)->until(
+    WebDriverExpectedCondition::visibilityOfElementLocated(WebDriverBy::cssSelector($playButtonSelector))
+);
+
+echo "网址：" . $driver->getCurrentURL() . "'\n";
+echo "标题：" . $driver->getTitle() . "'\n";
+
+// 点击播放按钮
+$playButton = $driver->findElement(WebDriverBy::cssSelector($playButtonSelector));
+$playButton->click();
+
+// 等待加载（方法不好）。可能有更好的方法，参考：
+// https://github.com/php-webdriver/php-webdriver/wiki/HowTo-Wait
+sleep(5);
+
+// 从网络日志中寻找视频文件
+$logs = $driver->manage()->getLog('performance');
+
+echo "视频文件（可能不完整，只是开头一部分）：\n";
+foreach ($logs as $log) {
+    $msg = json_decode($log['message']);
+    if (empty($msg) || empty($msg->message) || empty($msg->message->params)
+        || empty($msg->message->params->request) || empty($msg->message->params->request->url)) {
+        continue;
+    }
+    $url = $msg->message->params->request->url;
+    if (preg_match($findUrl, $url)) {
+        echo "$url\n";
+    }
+}
+
+// 关闭浏览器
+$driver->quit();
